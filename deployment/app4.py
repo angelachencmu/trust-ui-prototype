0ca00eebc8c40931605510266764d05769bbd4f5
angelachencmu <117086946+angelachencmu@users.noreply.github.com>
2024-03-27 23:33:56 -0400

    Update app4.py

--- deployment/app4.py
+++ deployment/app4.py
@@ -8,7 +8,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.tree import export_graphviz
 from sklearn.neighbors import KNeighborsClassifier
 
-st.title('Iris')
+st.title('Iris Classifier')
 
 # Load the Iris dataset
 iris = load_iris()
@@ -21,7 +21,7 @@ df['variety'] = pd.Categorical.from_codes(iris.target, iris.target_names)
 if st.checkbox('Show dataframe'):
     st.write(df)
 
-st.subheader('Machine Learning models')
+st.subheader('Machine Learning Models')
 
 # Allow users to choose features to train
 selected_features = st.multiselect('Select features to train', iris.feature_names)
