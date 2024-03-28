841fd01dc86f89460d50528152df6b763d0831e2
angelachencmu <117086946+angelachencmu@users.noreply.github.com>
2024-03-27 22:03:51 -0400

    Update app4.py

--- deployment/app4.py
+++ deployment/app4.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from sklearn.datasets import load_iris
 
- 
 st.title('Iris')
 
 # Load the Iris dataset
@@ -16,63 +15,59 @@ iris = load_iris()
 # Create a DataFrame
 df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 df['target'] = iris.target
- 
+df['variety'] = pd.Categorical.from_codes(iris.target, iris.target_names)
+
 if st.checkbox('Show dataframe'):
     st.write(df)
- 
+
 st.subheader('Scatter plot')
- 
 species = st.multiselect('Show iris per variety?', df['variety'].unique())
 col1 = st.selectbox('Which feature on x?', df.columns[0:4])
 col2 = st.selectbox('Which feature on y?', df.columns[0:4])
- 
 new_df = df[(df['variety'].isin(species))]
 st.write(new_df)
+
 # create figure using plotly express
-fig = px.scatter(new_df, x =col1,y=col2, color='variety')
+fig = px.scatter(new_df, x=col1, y=col2, color='variety')
+
 # Plot!
- 
- 
 st.plotly_chart(fig)
- 
+
 st.subheader('Histogram')
- 
 feature = st.selectbox('Which feature?', df.columns[0:4])
+
 # Filter dataframe
 new_df2 = df[(df['variety'].isin(species))][feature]
 fig2 = px.histogram(new_df, x=feature, color="variety", marginal="rug")
 st.plotly_chart(fig2)
- 
+
 st.subheader('Machine Learning models')
- 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
- 
- 
-features= df[['sepal.length', 'sepal.width', 'petal.length', 'petal.width']].values
+
+features = df[['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']].values
 labels = df['variety'].values
- 
-X_train,X_test, y_train, y_test = train_test_split(features, labels, train_size=0.7, random_state=1)
- 
+
+X_train, X_test, y_train, y_test = train_test_split(features, labels, train_size=0.7, random_state=1)
+
 alg = ['Decision Tree', 'Support Vector Machine']
 classifier = st.selectbox('Which algorithm?', alg)
-if classifier=='Decision Tree':
+
+if classifier == 'Decision Tree':
     dtc = DecisionTreeClassifier()
     dtc.fit(X_train, y_train)
     acc = dtc.score(X_test, y_test)
     st.write('Accuracy: ', acc)
     pred_dtc = dtc.predict(X_test)
-    cm_dtc=confusion_matrix(y_test,pred_dtc)
+    cm_dtc = confusion_matrix(y_test, pred_dtc)
     st.write('Confusion matrix: ', cm_dtc)
- 
-     
 elif classifier == 'Support Vector Machine':
-    svm=SVC()
+    svm = SVC()
     svm.fit(X_train, y_train)
     acc = svm.score(X_test, y_test)
     st.write('Accuracy: ', acc)
     pred_svm = svm.predict(X_test)
-    cm=confusion_matrix(y_test,pred_svm)
+    cm = confusion_matrix(y_test, pred_svm)
     st.write('Confusion matrix: ', cm)
